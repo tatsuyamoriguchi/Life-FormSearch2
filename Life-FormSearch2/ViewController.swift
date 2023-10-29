e5a4eb06f35d0d6aeac38758a629096511feab2f
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2023-10-29 09:38:03 -0700

    Initial Commit

---
+++ Life-FormSearch2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Life-FormSearch2
+//
+//  Created by Tatsuya Moriguchi on 10/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
