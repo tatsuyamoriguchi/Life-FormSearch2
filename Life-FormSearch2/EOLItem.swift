b0d37c7be19c4dc1a9e5305adb6a44d71b459a4e
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2023-10-29 23:32:02 -0700

    Added EOLItem.swift

---
+++ Life-FormSearch2/EOLItem.swift
@@ -0,0 +1,20 @@
+//
+//  EOLItem.swift
+//  Life-FormSearch2
+//
+//  Created by Tatsuya Moriguchi on 10/29/23.
+//
+
+import Foundation
+
+struct EOLItem: Codable {
+    var id: Int
+    var title: String
+    var content: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title = "scientificName"
+        case content = "commonName"
+    }
+}
